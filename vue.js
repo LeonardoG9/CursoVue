0b9bc114bdf270e868f14e7deca143759feeb00d
LeonardoG9 <jose.guzman@cceo.com.mx>
2021-09-29 12:25:22 -0500

    9no Commit

--- vue.js
+++ vue.js
@@ -2,13 +2,17 @@ new Vue({
   el: "#eventos",
   data:{
     x:0,
-    y:0
+    y:0,
+    contador:0
   },
   methods:{
    showCoord:function(evento){
     this.x=evento.clientX;
     this.y= evento.clientY;
 
+   },
+   sumar: function(cantidad){
+    this.contador+=cantidad
    }
    }
   }
