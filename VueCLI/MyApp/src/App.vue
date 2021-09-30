097ededc77ab370806fb3546bf08b90c1ad1b3e5
LeonardoG9 <jose.guzman@cceo.com.mx>
2021-09-30 13:47:47 -0500

    SlotsIntroduccion

--- VueCLI/MyApp/src/App.vue
+++ VueCLI/MyApp/src/App.vue
@@ -1,55 +1,20 @@
 <template>
-  <div id="app" class="container">
+  <div class="container">
     <div class="bg-light p-5 rounded-lg m-3">
-      <titulo :titulo="Titulo" :numTasks="numTasks"></titulo>
-
-      <add-task :tareas="tareas" :actualizarCount="actualizarCount"></add-task>
-      <task-list v-if="tareas.length>0" :tareas="tareas"></task-list>
-      <p v-else>
-        No tienes tareas pendientes!
-      </p>
+      <formulario></formulario>
     </div>
   </div>
 </template>
 
 <script>
-import Titulo from "./Titulo.vue";
-import AddTask from "./AddTask.vue";
-import TaskList from "./TaskList.vue";
-import { bus } from "./main";
+import Formulario from './Formulario.vue'
 export default {
-  components: {
-    Titulo,
-    AddTask,
-    TaskList,
-  },
-  data() {
-    return {
-      Titulo: "Lista de tareas",
-      numTasks: 3,
-      tareas: [
-        {
-          task: "Aprender Vue.JS",
-          done: false,
-        },
-        {
-          task: "Aprender Laravel",
-          done: false,
-        },
-        {
-          task: "Aprender Mongo",
-          done: false,
-        },
-      ],
-    };
-  },
-  methods: {
-    actualizarCount() {
-      this.numTasks++;
-    },
-  },
-};
+  components:{
+    Formulario
+  }
+}
 </script>
 
 <style>
+
 </style>
---
+++ VueCLI/MyApp/src/Formulario.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="form">
+    <h3>Iniciar sesión</h3>
+    <div class="form-group">
+      <label for="correo">Correo electrónico</label>
+      <input
+        type="email"
+        id="correo"
+        class="form-control"
+        placeholder="@Email"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="clave">Correo electrónico</label>
+      <input
+        type="password"
+        id="clave"
+        class="form-control"
+        placeholder="Password"
+      />
+    </div>
+    <br>
+    <button class="btn btn-primary">Iniciar sesión</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
